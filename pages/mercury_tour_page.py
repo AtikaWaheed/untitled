19cacc664fad221d76642362a7858004fbe7aecc
AtikaWaheed <atikawaheed@hotmail.com>
2017-12-22 17:03:46 +0500

    Making test more clear to verify things

--- pages/mercury_tour_page.py
+++ pages/mercury_tour_page.py
@@ -1,21 +1,30 @@
 from selenium.webdriver.support.select import Select
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
 
 
 class BasePage(object):
 
     def __init__(self, driver):
-        # instantiate driver
+        """
+        instantiate driver
+        """
         self.driver = driver
 
 
 class MainWelcomePage(BasePage):
 
     def title_matches(self):
-        # Find out Header
+        """
+        Make sure that title matches
+        """
         return "Welcome: Mercury Tours" in self.driver.title
 
     def login_cred_submit(self, username, password):
-        # Give login cred and click submit
+        """
+        Submit username and password in fields and submit
+        """
         user_name = self.driver.find_element_by_name('userName')
         user_name.send_keys(username)
         password_keys = self.driver.find_element_by_name('password')
@@ -23,62 +32,99 @@ class MainWelcomePage(BasePage):
         submit = self.driver.find_element_by_name('submit')
         submit.click()
 
+    def get_logged_in(self):
+        """
+        Make sure it gets loggedOut
+        """
+        return self.driver.find_element_by_css_selector('img[src="images/banner2.gif"]')
+
     def flights_ver_click(self):
-        # Move to flights vertical
+        """
+        Moving to flights vertical
+        """
         self.driver.find_element_by_link_text('Flights').click()
 
     def pass_count(self, passcount):
-        #Select pass_count
+        """
+        Enter any passcount in passenger field
+        """
         passenger = self.driver.find_element_by_name('passCount')
         Select(passenger).select_by_visible_text(passcount)
+        return passcount
 
     def from_port(self, fromport):
-        #Select from_port
+        """
+        Enter from_port from field
+        """
         departing_port = self.driver.find_element_by_name('fromPort')
         Select(departing_port).select_by_visible_text(fromport)
+        return fromport
 
     def from_month(self, frommonth):
-        #Select from_month
+        """
+        Select from_month from dropdown
+        """
         depart_month = self.driver.find_element_by_name('fromMonth')
         Select(depart_month).select_by_visible_text(frommonth)
+        return frommonth
 
     def from_day(self, fromday):
-        #Select from_day
+        """
+        Select from_day from dropdown
+        """
         depart_day = self.driver.find_element_by_name('fromDay')
         Select(depart_day).select_by_visible_text(fromday)
+        return fromday
 
     def to_port(self, toport):
-        #Select to_port
+        """
+        Select to_port
+        """
         arriving_in = self.driver.find_element_by_name('toPort')
         Select(arriving_in).select_by_visible_text(toport)
+        return toport
 
     def to_month(self, tomonth):
-        #Select to_month
+        """
+        Select to_month from list
+        """
         return_month = self.driver.find_element_by_name('toMonth')
         Select(return_month).select_by_visible_text(tomonth)
+        return tomonth
 
     def to_day(self, today):
-        #Select to_day
+        """
+        Select to_day from list
+        """
         return_date = self.driver.find_element_by_name('toDay')
         Select(return_date).select_by_visible_text(today)
+        return today
 
     def sevice_cred(self):
-        #Choose any service
+        """
+        Select any Business class from dropdown
+        """
         self.driver.find_element_by_css_selector('input[value="Business"]').click()
 
     def airline_name_select(self, airline):
-        #Select an airline
+        """
+        Choose an airline
+        """
         airline_name = self.driver.find_element_by_css_selector('select[name="airline"]')
         Select(airline_name).select_by_visible_text(airline)
+        return airline
 
     def flight_det_sub(self):
-        #Found flight details
+        """
+        After entering all credentials submit form and wait to open next page
+        """
         self.driver.find_element_by_css_selector('input[name="findFlights"]').click()
-
-    def hotels_ver_click(self):
-        # Click hotels vertical
-        self.driver.find_element_by_link_text('Hotels').click()
+        WebDriverWait(self.driver, 10).until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, 'img[src="images/home.gif"]'))
+        )
 
     def back_click(self):
-        #Moving back to FD
+        """
+        Moving back to front page
+        """
         self.driver.find_element_by_css_selector('img[src="images/home.gif"]').click()
--- tests/test_mercury_tour.py
+++ tests/test_mercury_tour.py
@@ -6,59 +6,114 @@ from pages import mercury_tour_page
 class MercuryMainPage(unittest.TestCase):
 
     def setUp(self):
+        """
+        Setup driver and initialize class
+        """
         self.driver = webdriver.Firefox()
         self.driver.get("http://demo.guru99.com/selenium/newtours/")
         self.mt_page = mercury_tour_page.MainWelcomePage(self.driver)
 
-    def test_tiltle_matches(self):
+    def test_title_matches(self):
+        """
+        Verify correct page is open
+        """
         assert self.mt_page.title_matches()
-        self.mt_page.title_matches()
 
     def test_login(self):
+        """
+        Verify that user can log in entering valid username and valid password
+        """
         self.mt_page.login_cred_submit("admin", "admin")
+        assert self.mt_page.get_logged_in()
 
     def test_flights_ver_click(self):
+        """
+        Verify the exact flight page on display
+        """
         self.mt_page.flights_ver_click()
         assert self.driver.find_element_by_css_selector('img[src="images/mast_flightfinder.gif"]')
 
     def test_pass_count(self):
-        self.mt_page.pass_count("4")
+        """
+        Verify correct pass_count number has picked
+        """
+        count = self.mt_page.pass_count("4")
+        self.assertEqual(count, "4")
 
     def test_from_port(self):
-        self.mt_page.from_port("Frankfurt")
+        """
+        Verify correct from_port has picked
+        """
+        port = self.mt_page.from_port("Frankfurt")
+        self.assertEqual(port, "Frankfurt")
 
     def test_from_month(self):
-        self.mt_page.from_month("September")
+        """
+        Verify correct from_month has picked
+        """
+        f_month = self.mt_page.from_month("September")
+        self.assertEqual(f_month, "September" )
 
     def test_from_day(self):
-        self.mt_page.from_day("5")
+        """
+        Verify correct from_day has picked
+        """
+        f_day = self.mt_page.from_day("5")
+        self.assertEqual(f_day, "5")
 
     def test_to_port(self):
-        self.mt_page.to_port("London")
+        """
+        Verify correct to_port has picked
+        """
+        t_port = self.mt_page.to_port("London")
+        self.assertEqual(t_port, "London")
 
     def test_to_month(self):
-        self.mt_page.to_month("March")
+        """
+        Verify correct to_month has picked
+        """
+        t_month = self.mt_page.to_month("March")
+        self.assertEqual(t_month, "March")
 
     def test_to_day(self):
-        self.mt_page.to_day("5")
+        """
+        Verify correct to_day has picked
+        """
+        t_day = self.mt_page.to_day("5")
+        self.assertEqual(t_day, "5")
 
     def test_sevice_cred(self):
-        self.mt_page.sevice_cred()
+        """
+        Verify service_cred has picked correct and selected also
+        """
+        s_cred = self.mt_page.sevice_cred()
+        assert self.driver.find_element_by_css_selector('input[value="Business"]').text() == "Business class "
 
     def test_airline_name_select(self):
-        self.mt_page.airline_name_select("Blue Skies Airlines")
+        """
+        Verify correct airline has picked and selected
+        """
+        air_name = self.mt_page.airline_name_select("Blue Skies Airlines")
+        self.assertEqual(air_name, "Blue Skies Airlines")
 
     def test_flight_det_sub(self):
-        self.mt_page.flight_det_sub()
-
-    def test_hotels_ver_click(self):
-        self.mt_page.hotels_ver_click()
+        """
+        Verify flight details has submitted
+        """
+        det_sub = self.mt_page.flight_det_sub()
+        self.assertTrue(det_sub)
 
     def test_back_click(self):
-        assert self.driver.find_element_by_css_selector('img[src="images/mast_construction.gif"]')
+        """
+        Verify we have moved to front page
+        """
         self.mt_page.back_click()
+        assert self.mt_page.title_matches()
 
     def tearDown(self):
+        """
+        Verify page has closed
+        """
         self.driver.close()
 
 if __name__ == "__main__":
